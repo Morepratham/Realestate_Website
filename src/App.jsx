6ebc5382b8a2a951e716dc0afc4d15ae5a35970b
Pratham <prathamwork207@gmail.com>
2024-04-10 15:41:36 +0530

    Real Estate website

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Hero from "./components/Hero/Hero";
 import './App.css'
 import Companies from "./components/Companies/Companies";
 import Residency from "./components/Residency/Residency";
+import Value from "./components/Value/Value";
+// import Value from "./components/Value/Value";
 function App() {
   return (
     <div className="App">
@@ -13,6 +15,7 @@ function App() {
       </div>
       <Companies/>
       <Residency/>
+      <Value/> 
     </div>
   );
 }
